06de81e309b18af47cacf8603b69ac38bb06cfbc
judehl <87223822+judehl@users.noreply.github.com>
2022-01-06 21:22:37 +0800

    Delete example3 directory

--- example3/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './components/Example4-9'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
